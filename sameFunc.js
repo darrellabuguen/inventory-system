b9027e6c4b68f4aba9526a4a2794ff000b5be31f
Darrel <darrellabuguen2@gmail.com>
2024-03-07 21:14:39 +0800

    fix the showiteminfo not getting accessed when item info button is clicked

--- sameFunc.js
+++ sameFunc.js
@@ -1,3 +1,18 @@
+
+import firebaseConfig from "./config.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, deleteUser, GoogleAuthProvider, signInWithPopup, linkWithPopup } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+import {
+    getDatabase, ref, push, onValue, child,
+    remove, set, query, orderByChild, get,
+    startAt, endAt, limitToLast
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-database.js";
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase(app);
+
 var selected_item = null;
 
 export function openAdd() {
@@ -30,9 +45,9 @@ export function showItemOption(id) {
     if (status) {
         var div = document.createElement("div");
         var del_span = document.createElement("span");
-        var del_btn = `<button id="${id}" class="delete-item">Delete</button>`
+        var del_btn = `<button class="delete-item">Delete</button>`
         var info_span = document.createElement("span");
-        var info_btn = `<button id="${id}" class="item-info">Item Info</button>`
+        var info_btn = `<button class="item-info">Item Info</button>`
         div.classList.add("item-option-container");
 
         del_span.innerHTML = del_btn;
@@ -43,6 +58,19 @@ export function showItemOption(id) {
         item_info_container.append(div);
         item_info_container.classList.remove("inactive");
     }
+
+    //select the class for the created button options
+    const item_info = document.querySelector(".item-info");
+    const delete_item = document.querySelector(".delete-item");
+
+    //add listener when clicked
+    item_info.addEventListener("click", () => {
+        showItemInfo(id);       //function that would show the information of selected item
+    })
+
+    delete_item.addEventListener("click", () => {
+        deleteItem(id);         //function for deleting the selected item
+    })
 }
 
 export function hideContainers() {
@@ -58,6 +86,27 @@ export function hideContainers() {
         item_option.classList.remove("opened");
         selected_item = null;
     }
+}
+
+function showItemInfo(id) {
+    const userid = auth.currentUser.uid;
+    const refr = query(ref(db, `User/${userid}/Items/${id}`));
+    get(refr)
+        .then((snapshot) => {
+            const data = snapshot.val();
+            var item_name = data.item_name;
+            var item_batch = data.batch_no;
+            var item_qnty = data.item_qnty;
+            var item_price = data.item_price;
+            var item_mdate = data.manufactured;
+            var item_edate = data.expiry;
+            var item_added = data.added;
+            console.log(item_name);
+        })
+        .catch((err) => {
+            alert(err);
+            console.error(err);
+        })
 }
 
 document.addEventListener("DOMContentLoaded", () => {
